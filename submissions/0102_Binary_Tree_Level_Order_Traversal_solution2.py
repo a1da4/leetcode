64a345be815482f67d86a68c04c6191035d51c2a
a1da4 <ai.the.taichi@gmail.com>
2023-05-11 10:23:29 +0900

    solve 0102 w/ breadth-first search

---
+++ submissions/0102_Binary_Tree_Level_Order_Traversal_solution2.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        
+        queue = [root]
+        result = []
+
+        while queue:
+            depth = []
+            depth_size = len(queue)
+ 
+            for current_depth in range(depth_size):
+                node = queue.pop(0)
+                depth.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            result.append(depth)
+
+        return result
