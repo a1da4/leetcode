c56abca6d5590abdaf7bd740909327f1b6b6120b
a1da4 <ai.the.taichi@gmail.com>
2023-09-04 09:07:28 +0900

    solve 1268

---
+++ submissions/1268_Search_Suggestions_System.py
@@ -0,0 +1,15 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+        answer = []
+        currCands = products
+        currWord = ""
+        currLength = 0
+        for i in range(len(searchWord)):
+            currWord += searchWord[i]
+            currLength += 1
+            currCands = [cand for cand in products if cand[:currLength]==currWord]
+
+            answer.append(currCands[:3])
+
+        return answer
