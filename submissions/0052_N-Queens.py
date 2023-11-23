33a06bcbba9373e673b784789ed51a7dd31579b7
a1da4 <ai.the.taichi@gmail.com>
2023-11-23 17:15:29 +0900

    solve 0052

---
+++ submissions/0052_N-Queens.py
@@ -0,0 +1,36 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def isSafe(rowId: int, colId: int, board: List[List[str]]) -> bool:
+            _rowId, _colId = rowId, colId
+            while _rowId >= 0 and _colId >= 0:
+                if board[_rowId][_colId] == "Q":
+                    return False
+                _rowId -= 1
+                _colId -= 1 
+            _rowId, _colId = rowId, colId
+            while _rowId < n and _colId >= 0:
+                if board[_rowId][_colId] == "Q":
+                    return False
+                _rowId += 1
+                _colId -= 1
+            while colId >= 0:
+                if board[rowId][colId] == "Q":
+                    return False
+                colId -= 1
+            return True
+        
+        def search(colId: int, board: List[List[str]]) -> None:
+            nonlocal answer
+            if colId == n:
+                answer += 1
+                return
+            for rowId in range(n):
+                if isSafe(rowId, colId, board):
+                    board[rowId] = board[rowId][:colId] + "Q" + board[rowId][colId+1:]
+                    search(colId + 1, board)
+                    board[rowId] = board[rowId][:colId] + "#" + board[rowId][colId+1:]
+
+        answer = 0
+        board = ["#" * n for _ in range(n)]
+        search(0, board)
+        return answer
