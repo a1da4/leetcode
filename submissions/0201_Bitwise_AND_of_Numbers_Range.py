4aed1aed4b88fce52931909e27c39503fb798bea
a1da4 <ai.the.taichi@gmail.com>
2023-12-08 11:14:28 +0800

    solve 0201

---
+++ submissions/0201_Bitwise_AND_of_Numbers_Range.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        if left == 0:
+          return 0
+        
+        shift = 0
+        while left != right:
+            left = left >> 1
+            right = right >> 1
+        
+            shift += 1
+            print(shift, left, right)
+
+        return left << shift
