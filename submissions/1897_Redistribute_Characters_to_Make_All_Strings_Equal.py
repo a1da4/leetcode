cee3e3552342cf881054c3b4ed19cc75cde088f3
a1da4 <ai.the.taichi@gmail.com>
2023-12-30 12:47:37 +0900

    solve 1897 (daily: 2023-12-30)

--- submissions/1897_Redistribute_Characters_to_Make_All_Strings_Equal.py
+++ submissions/1897_Redistribute_Characters_to_Make_All_Strings_Equal.py
@@ -2,7 +2,6 @@ class Solution:
     def makeEqual(self, words: List[str]) -> bool:
         numWords = len(words)
         wordFreq = Counter("".join(words))
-
         for word, freq in wordFreq.items():
             if freq % numWords > 0:
                 return False
