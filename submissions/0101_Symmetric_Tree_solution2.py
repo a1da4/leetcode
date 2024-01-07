ff081cd4b6c080513528f3cec5ff089e3077d971
a1da4 <ai.the.taichi@gmail.com>
2024-01-07 12:28:58 +0900

    solve 0101: 38ms -> 35ms

---
+++ submissions/0101_Symmetric_Tree_solution2.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        leftNodes = deque([root])
+        rightNodes = deque([root])
+
+        while leftNodes and rightNodes:
+            numNodes = len(leftNodes)
+            for _ in range(numNodes):
+                leftNode = leftNodes.popleft()
+                rightNode = rightNodes.popleft()
+
+                if leftNode.left is None and rightNode.right is None:
+                    pass
+                elif leftNode.left is None or rightNode.right is None:
+                    return False
+                elif leftNode.left.val == rightNode.right.val: 
+                    leftNodes.append(leftNode.left)
+                    rightNodes.append(rightNode.right)
+                else:
+                    return False
+
+                if leftNode.right is None and rightNode.left is None:
+                    pass
+                elif leftNode.right is None or rightNode.left is None:
+                    return False
+                elif leftNode.right.val == rightNode.left.val:
+                    leftNodes.append(leftNode.right)
+                    rightNodes.append(rightNode.left)
+                else:
+                    return False
+        return True
