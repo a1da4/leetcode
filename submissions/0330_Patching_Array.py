e22f885163d0944c7032a2c43ea7f99c22b8a4f3
a1da4 <ai.the.taichi@gmail.com>
2024-06-16 14:13:11 +0900

    solve 0330 (daily: 2024-06-16)

---
+++ submissions/0330_Patching_Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        answer = 0
+        i = 0
+        miss = 1
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                answer += 1
+        return answer
+
