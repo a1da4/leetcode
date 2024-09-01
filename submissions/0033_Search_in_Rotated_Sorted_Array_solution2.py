0674d42825bab647041a8d3895147e46ae80e6ca
a1da4 <ai.the.taichi@gmail.com>
2024-09-01 18:44:51 +0900

    solve 0033: use binary search, 50ms -> 46ms

--- submissions/0033_Search_in_Rotated_Sorted_Array_solution2.py
+++ submissions/0033_Search_in_Rotated_Sorted_Array_solution2.py
@@ -1,6 +1,28 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target not in set(nums):
+        N = len(nums)
+        left, right = 0, N - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[-1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        def _binarySearch(left, right):
+            while left <= right:
+                mid = (left + right) // 2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             return -1
-        else:
-            return nums.index(target)
+        
+        targetId = _binarySearch(0, left - 1)
+        if targetId != -1:
+            return targetId
+        
+        return _binarySearch(left, N - 1)
