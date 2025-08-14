4ba9838d7931f11bb384ce812da1b77f17610069
a1da4 <ai.the.taichi@gmail.com>
2025-08-14 09:15:34 +0900

    solve 2264 (daily: 2025-08-14)

---
+++ submissions/2264_Largest_3-Same-Digit_Number_in_String.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        for digit in range(9, -1, -1):
+            target = str(digit) * 3
+            if target in num:
+                return target
+        return ""
